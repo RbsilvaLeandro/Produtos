1a795496852da2116a0ecfe555c35b0544c39694
RbsilvaLeandro <rbsilva.leandro@gmail.com>
2022-07-16 21:44:22 -0300

    Adicionando controller - swagger - connection database

---
+++ Produtos.API/Controllers/ProdutosController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Produtos.Application.DTO;
+using Produtos.Application.Interfaces;
+using System;
+
+namespace Produtos.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProdutosController : Controller
+    {
+        private readonly IApplicationProdutoService _applicationServiceProduto;
+        public ProdutosController(IApplicationProdutoService applicationServiceProduto)
+        {
+            _applicationServiceProduto = applicationServiceProduto;
+        }
+
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return Ok(_applicationServiceProduto.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return Ok(_applicationServiceProduto.GetById(id));
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO == null)
+                    return NotFound();
+
+                _applicationServiceProduto.Add(produtoDTO);
+                return Ok("Produto cadastrado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO == null)
+                    return NotFound();
+
+                _applicationServiceProduto.Update(produtoDTO);
+                return Ok("Produto atualizado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        [HttpDelete()]
+        public ActionResult Delete([FromBody] ProdutoDTO produtoDTO)
+        {
+            try
+            {
+                if (produtoDTO == null)
+                    return NotFound();
+
+                _applicationServiceProduto.Remove(produtoDTO);
+                return Ok("Produto excluido com sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
--- Produtos.API/Startup.cs
+++ Produtos.API/Startup.cs
@@ -1,9 +1,14 @@
+using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Produtos.Infra.CrossCutting.IOC;
+using Produtos.Infra.Data;
 
 namespace Produtos.API
 {
@@ -19,12 +24,25 @@ namespace Produtos.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = Configuration["SqlConnection:SqlConnectionString"];
+            services.AddDbContext<SqlContext>(options => options.UseSqlServer(connection));
+
+            services.AddControllers();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+
+            services.AddRazorPages();
+
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Produtos.API", Version = "v1" });
             });
+        }
+        public void ConfigureContainer(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new ModuleIOC());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,15 +51,30 @@ namespace Produtos.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Produtos.API v1"));
             }
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ProjetoModeloDDD"));
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseRouting();
+
+            app.UseCors(builder => builder
+                     .AllowAnyHeader()
+                     .AllowAnyMethod()
+                     .SetIsOriginAllowed((host) => true)
+                     .AllowCredentials()
+                     );
+
+            app.UseAuthorization();
+
 
             app.UseEndpoints(endpoints =>
             {
